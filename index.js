4ae763fa94d8ca0c3d2c0cd7245cba22088b17b4
Jordan Sharpe <jord.sharpe@gmail.com>
2021-08-27 23:34:40 +0100

    Stared work on layouts and partials

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp', {
     useNewUrlParser: true,
@@ -23,6 +24,7 @@ db.once('open', () => {
 const app = express();
 const PORT = 3000;
 
+app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
