272b99ee04a9e442e261809eb17206cc38a4eee5
Jordan Sharpe <jord.sharpe@gmail.com>
2021-08-30 22:05:56 +0100

    Added bootstrap styles to views

--- seeds/index.js
+++ seeds/index.js
@@ -15,14 +15,17 @@ db.once('open', () => {
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
-
+const price = Math.floor(Math.random() * 20 ) + 10;
 const seedDB = async ( ) => {
     await Campground.deleteMany({});
     for( let i = 0; i < 50 ; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi debitis explicabo veritatis sit nulla, nesciunt officiis dolores voluptate dolorem adipisci inventore? Sit distinctio assumenda qui, autem fugit cupiditate officia nihil.',
+            price
         })
         await camp.save();
     }
